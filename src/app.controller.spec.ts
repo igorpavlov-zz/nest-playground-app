f2589bfbf2452628cb1534e7bdd5415983775645
Igor Pavlov <pavlov.org@gmail.com>
2019-09-27 10:21:19 +0300

    Create custom root greeting

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should start with "Welcome"', () => {
+      expect(appController.getGreeting()).toMatch(/^Welcome/);
     });
   });
 });
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getGreeting(): string {
+    return 'Welcome to Igor Pavlov\'s Nest Playground App';
   }
 }
